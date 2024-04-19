6eebf39c720169a18f0ae1293e46c59569216c88
kudlaynj <167016811+kudlaynj@users.noreply.github.com>
2024-04-19 09:57:08 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,8 @@
+mi_string = 'all kids loves a mults'
+print(mi_string)
+print (len('all kids loves a mults'))
+print ('all kids loves a mults'.upper())
+print ('all kids loves a mults'.lower())
+print ('all kids loves a mults'.replace(' ',''))
+print(mi_string[0])
+print(mi_string[21])
