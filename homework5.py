6387f2c08382c45374b7e9d8f53869c788e412da
kudlaynj <167016811+kudlaynj@users.noreply.github.com>
2024-04-22 10:30:37 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,13 @@
+my_list = {'table': 68, 'chair': 136, 'bed': 33, 'sofa': 31, 'stool': 124}
+print(my_list)
+print('table', 'stool')
+print(my_list['bed'])
+print(my_list['sofa'])
+print(my_list['stool'])
+my_list['bed'] = 86
+print(my_list)
+
+my_dict = {'table': 'стол', 'chair': 'стул', 'bed': 'кровать', 'sofa': ['софа', 'кушетка'], 'stool': 'табурет'}
+print(my_dict)
+print(my_dict.values())
+
