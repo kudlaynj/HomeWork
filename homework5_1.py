89a3ff189aa80d5563c3bf9675d2a4dfeea29a31
kudlaynj <167016811+kudlaynj@users.noreply.github.com>
2024-04-25 09:56:22 +0300

    Add files via upload

---
+++ homework5_1.py
@@ -0,0 +1,13 @@
+my_list = ["boy", "girl", "kid", "man", "woman"]
+print(my_list)
+print(my_list[0], my_list[-1])
+print(my_list[2:5])
+my_list[2] = "teenager"
+print(my_list)
+
+my_dict = {'one': 1, 'two': 2, 'three': 3}
+print(my_dict)
+print(my_dict.values())
+my_dict.update({'four': 4})
+print(my_dict)
+
