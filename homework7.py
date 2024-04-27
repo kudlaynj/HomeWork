0d85ce395773e3a903ebc13a9bee27ea5c7250f1
kudlaynj <167016811+kudlaynj@users.noreply.github.com>
2024-04-27 10:47:28 +0300

    Add files via upload

---
+++ homework7.py
@@ -0,0 +1,8 @@
+list_car = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+for i in list_car:
+    print( "Я езжу на автомобиле марки ", i)
+cars_count = 0
+for car in list_car:
+    print(f'Я езжу на автомобиле марки : {car}')
+    cars_count += 10
+    print(cars_count)
