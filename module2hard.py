8c2342d77a5096597c1baa04f0f760468179a321
kudlaynj <167016811+kudlaynj@users.noreply.github.com>
2024-05-05 14:36:32 +0300

    Add files via upload

---
+++ module2hard.py
@@ -0,0 +1,13 @@
+def generate_password(n):
+    result = ""
+    for i in range(1, n):
+        for j in range(i + 1, n + 1):
+            if n % (i + j) == 0:
+                result += str(i) + str(j)
+    return result
+
+
+
+for n in range(3, 21):
+    password = generate_password(n)
+    print(f"Пароль для числа {n}: {password}")
