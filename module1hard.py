2e1f16de74e2dda4ea6697f72be87f142bfd4f24
kudlaynj <167016811+kudlaynj@users.noreply.github.com>
2024-05-10 20:47:21 +0300

    Add files via upload

--- module1hard.py
+++ module1hard.py
@@ -1,6 +1,4 @@
 students = {'Johnni', 'Bilbo', 'Steve', 'Kendrik', 'Aaron'}
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
-average = sum(grades[0]) / len(grades), sum(grades[1]) / len(grades), sum(grades[2]) / len(grades), sum(
-    grades[3]) / len(grades), sum(grades[4]) / len(grades)
-journal = zip(sorted(students), average)
-print(dict(journal))
+average_grades = {student: sum(grade) / len(grade) for student, grade in zip(sorted(students), grades)}
+print(average_grades)
