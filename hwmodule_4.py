92038b3de831ae6e271f282b5be81b0de759616b
kudlaynj <167016811+kudlaynj@users.noreply.github.com>
2024-05-10 17:42:05 +0300

    Add files via upload

---
+++ hwmodule_4.py
@@ -0,0 +1,11 @@
+def look():
+    print("Hello, world")
+
+
+def look_1():
+    print("Hello, world")
+
+
+look()
+
+look_1()
--- main.py
+++ main.py
@@ -1,16 +1,1 @@
-def test():
-    a = 12
-    b = 25
-    print(a, b)
-
-
-def test2(n):
-    if n == 0:
-        return 0
-    else:
-        return n + test2(n - 1)
-
-
-test()
-
-print(test2(7))
+import hwmodule_4
