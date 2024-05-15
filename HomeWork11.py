7933fe40278615dff1e10448245475340137f81b
kudlaynj <167016811+kudlaynj@users.noreply.github.com>
2024-05-15 10:03:37 +0300

    Add files via upload

---
+++ HomeWork11.py
@@ -0,0 +1,11 @@
+def test_function():
+    def inner_function():
+        a = "Я в области видимости функции test_function"
+        print(a)
+    inner_function()
+
+
+test_function()
+inner_function()
+
+
