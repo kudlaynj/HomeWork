05defc1ad949a711cc0dbb7fe66c5fcd9f4e05d6
kudlaynj <167016811+kudlaynj@users.noreply.github.com>
2024-05-26 18:03:50 +0300

    Add files via upload

--- homework14.py
+++ homework14.py
@@ -2,6 +2,10 @@ class Buiding:
     def __init__(self):
         self.total = 0
 
+    def home1(self):
+        for i in range(1, 41):
+            print('Лифт проезжает этаж № ', i)
 
-for i in range(1, 41):
-    print('Лифт проезжает этаж № ', i)
+
+my_home = Buiding
+my_home.home1(int)
