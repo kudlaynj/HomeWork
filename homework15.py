839a2f431e1672ac55f08082ccc22f1a7cf58e9d
kudlaynj <167016811+kudlaynj@users.noreply.github.com>
2024-06-03 12:24:57 +0300

    Add files via upload

---
+++ homework15.py
@@ -0,0 +1,37 @@
+class Car:
+    price = 1000000
+    hp = 152
+
+    def horse_powers(self):
+        print('Количество лошадиных сил = ', self.hp, 'Стоимость', self.price)
+
+
+class Nissan(Car):
+    price = 800000
+    hp = 124
+
+    def horse_powers(self):
+        print('Количество лошадиных сил = ', self.hp, 'Стоимость',  self.price)
+
+
+class Kia(Car):
+    price = 1200000
+    hp = 174
+
+    def horse_powers(self):
+        print('Количество лошадиных сил = ', self.hp, 'Стоимость', self.price)
+
+
+print("Автомобиль")
+car_1 = Car()
+car_1.horse_powers()
+
+print("Nissan")
+car_2 = Nissan()
+car_2.horse_powers()
+
+print("Kia")
+car_3 = Kia()
+car_3.horse_powers()
+
+
