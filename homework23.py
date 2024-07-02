e5f559b5c9e6e73bd3be105206bdc472de36db8a
kudlaynj <167016811+kudlaynj@users.noreply.github.com>
2024-07-02 16:07:35 +0300

    Add files via upload

--- homework23.py
+++ homework23.py
@@ -5,10 +5,13 @@ def all_variants(text):
     text = re.split("", text)
     for i in text:
         print(i)
-        if i == "abcdefghij":
+        if i == "abc":
             yield True
 
 
 a = all_variants("abc")
 for i in a:
     print(i)
+print("abc"[0:2])
+print("abc"[1:3])
+print("abc")
