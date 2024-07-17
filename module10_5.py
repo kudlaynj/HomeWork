8cccdbf7594039c6332c6c8b28b021f7b0047eca
kudlaynj <167016811+kudlaynj@users.noreply.github.com>
2024-07-17 16:34:05 +0300

    Add files via upload

---
+++ module10_5.py
@@ -0,0 +1,40 @@
+import multiprocessing
+from queue import Queue
+from time import sleep
+from threading import Thread, Lock
+
+
+class WarehouseManager:
+    def __init__(self, receipt, shipment):
+        self.data = {}
+        self.receipt = receipt
+        self.shipment = shipment
+        self.queue = Queue()
+        self.lock = Lock()
+        self.requests = [
+            ("product1", "receipt", 100),
+            ("product2", "receipt", 150),
+            ("product1", "shipment", 30),
+            ("product3", "receipt", 200),
+            ("product2", "shipment", 50)
+        ]
+
+    def process_request(self):
+        for product, action, amount in self.requests:
+            with self.lock:
+                if product not in self.data:
+                    self.data[product] = 0
+                if action == "receipt":
+                    self.data[product] += amount
+                elif action == "shipment":
+                    self.data[product] = max(0, self.data[product] - amount)
+
+    def run(self):
+        self.process_request()
+        print(self.data)
+
+
+if __name__ == '__main__':
+    manager = WarehouseManager(70, 300)
+    manager.run()
+    print(manager.data)
