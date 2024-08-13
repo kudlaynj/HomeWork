77cdb0fe31590639509633d4da94c9614af8333e
kudlaynj <167016811+kudlaynj@users.noreply.github.com>
2024-08-13 19:24:14 +0300

    Add files via upload

---
+++ module_13_2.py
@@ -0,0 +1,25 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+
+api = ""
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+# @dp.message_handler()
+# async  def all_message(message):
+#   print("Мы получили сообщение!")
+
+@dp.message_handler(text=['Urban', 'ff'])
+async def Urban_message(message):
+    print("Urban message")
+
+
+@dp.message_handler(commands=['start'])
+async def start_message(message):
+    print("Start message")
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
