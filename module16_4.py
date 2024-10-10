aa21b6baa14875874850e7ee39fd1383b7f65de8
kudlaynj <167016811+kudlaynj@users.noreply.github.com>
2024-10-10 19:31:49 +0300

    Update module16_4.py

--- module16_4.py
+++ module16_4.py
@@ -1,9 +1,8 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Path
 from pydantic import BaseModel
-from typing import List
+from typing import List, Annotated
 
 app = FastAPI()
-
 users = []
 
 
@@ -13,27 +12,45 @@ class User(BaseModel):
     age: int
 
 
-@app.get("/users")
-async def get_users() -> List[User]:
+@app.get("/")
+async def all_users() -> List[User]:
     return users
 
 
-@app.post("/users")
-async def post_user(username: str, age: int) -> User:
-    current_index = len(users)
-    users.append({"id": current_index, "username": username, "age": age})
-    return users[-1]
-
-
-@app.put("/users/{user_id}")
-async def update_user(user_id: int, username: str, age: int) -> str:
-    try:
-        user = users[user_id]
-        user["username"] = username
-        user["age"] = age
-        return f"The user {user_id} has been updated."
-    except IndexError:
+@app.get('/user/{user_id}')
+async def get_users(user_id: int):
+    user_index = None
+    for i in range(len(users)):
+        if users[i].id == user_id:
+            user_index = i
+            break
+    if user_index is not None:
+        return user_index
+    else:
         raise HTTPException(status_code=404, detail="User was not found")
+
+
+@app.post('/user/{username}/{age}')
+async def post_user(username, age) -> User:
+    if len(users) == 0:
+        user_id = 1
+    user_id = users[-1].id + 1 if users != [] else 1
+    user = User(id=user_id, username=username, age=age)
+    users.append(user)
+    return user
+
+
+@app.put('/user/{user_id}/{username}/{age}')
+async def update_user(username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username',
+                                                    example="UrbanUser")],
+                      age: Annotated[int, Path(ge=18, le=120, description="Enter age", exemple='24')],
+                      user_id: int = Path(ge=1, le=100, description="Enter User ID", example='16')) -> str:
+    for up_user in users:
+        if up_user.id == user_id:
+            up_user.username = username
+            up_user.age = age
+            return f"The user {user_id} is updated."
+    raise HTTPException(status_code=404, detail="User was not found")
 
 
 @app.delete("/users/{user_id}")
